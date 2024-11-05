095df011d1ef85a568150b12c23c3fc8218dc7c3
Anton21a <shpilman.1995@gmail.com>
2024-11-05 13:44:21 +0300

    Short description of working process

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ git pull origin main
 
 git add .
 
-commit_message=${1:-"Update files (note: check script.sh)"}
+commit_message=${1:-"Short description of working process"}
 git commit -m "$commit_message"
 
 git push origin main
